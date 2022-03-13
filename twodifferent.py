7afeb9bd298f64c25310c2fb130f3731da23584f
MichaelMeier2907 <100663243+MichaelMeier2907@users.noreply.github.com>
2022-03-13 15:09:53 +0100

    Update twodifferent.py
    modify

--- twodifferent.py
+++ twodifferent.py
@@ -1,1 +1,12 @@
-
+# lowest energy if the two of two qbits have the same value
+#import
+import dimod
+# assign to variable exactsolver
+exactsolver = dimod.ExactSolver()
+# QUBO a0 = -1, a1 = -1 and b12 = 2
+Q = {(0,0):-1,(1,1):-1,(0,1):2}
+# assign results
+results = exactsolver.sample_qubo(Q)
+# print results
+for sample, energy in (results.data(['sample', 'energy'])):
+  print(sample, energy)
