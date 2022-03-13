523c8d4af0325204f64b8e1085488fbd62f4e282
MichaelMeier2907 <100663243+MichaelMeier2907@users.noreply.github.com>
2022-03-13 14:22:21 +0100

    Update onehot.py
    modify code

--- onehot.py
+++ onehot.py
@@ -1,1 +1,13 @@
-
+# exactly one problem: one of three qbits has to be one
+# runs on the exact solver
+# import
+import dimod
+# assign variable exactsolver
+exactsolver = dimod.ExactSolver()
+# QUBO quadratic terms -1, couplers 2
+Q = {(0,0):-1,(0,1):2,(0,2):2,(1,1):-1,(1,2):2,(2, 2):-1}
+# assign results
+results = exactsolver.sample_qubo(Q)
+# print results
+for sample, energy in (results.data(['sample', 'energy'])):
+  print(sample, energy)
