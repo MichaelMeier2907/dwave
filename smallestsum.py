2821d0818d5a308953d99eb75a0cdd77596251ed
MichaelMeier2907 <100663243+MichaelMeier2907@users.noreply.github.com>
2022-03-13 16:22:51 +0100

    Update smallestsum.py

--- smallestsum.py
+++ smallestsum.py
@@ -2,15 +2,15 @@
 # objectiv = min(17 x0 + 19 X1 + 21 x2)
 # constraint (x0 + x1 + x2 - 2) = 0 or min((x0 + x1 + x2 - 2)2)
 # QUBO = min( (17 x0 + 19 x1 + 21 x2) + gamma (- 3 x0 - 3 x1 - 3 x2 + 2 x0 x1 + 2 x0 x2 + 2 x1 x2) )
-# try gamma = - 3
-# QUBO = min (26 x0 + 28 x1 + 30 x2 - 6 x0 x1 - 6 x0 x2 - 6 x1 x2)
+# try gamma = 3
+# QUBO = min (8 x0 + 10 x1 + 12 x2 + 6 x0 x1 + 6 x0 x2 + 6 x1 x2)
 # runs on the exact solver
 # import
 import dimod
 # assign variable exactsolver
 exactsolver = dimod.ExactSolver()
-# QUBO a0 = 26, a1 = 28, a2 = 30, b01 = - 6, b02 = - 6 and b12 = - 6
-Q = {(0,0):26,(0,1):-6,(0,2):-6,(1,1):28,(1,2):-6,(2, 2):30}
+# QUBO a0 = 8, a1 = 10, a2 = 12, b01 = 6, b02 = 6 and b12 = 6
+Q = {(0,0):8,(0,1):6,(0,2):6,(1,1):10,(1,2):6,(2, 2):12}
 # assign results
 results = exactsolver.sample_qubo(Q)
 # print results
