a987cea51dc9099ebc6e56d3b2e70c80cffe6d5b
MichaelMeier2907 <100663243+MichaelMeier2907@users.noreply.github.com>
2022-03-13 15:12:16 +0100

    Update twodifferentqpu.py

--- twodifferentqpu.py
+++ twodifferentqpu.py
@@ -1,1 +1,12 @@
-
+# lowest energy if two of two qbits have different values
+#import
+from dwave.system import EmbeddingComposite, DWaveSampler
+# define QUBO
+# a0 = -1, a1 = -1, b01 = 2 
+Q = {(0,0):-1,(1,1):-1,(0,1):2}
+# embedding
+sampler = EmbeddingComposite(DWaveSampler())
+# run sampler
+sampleset = sampler.sample_qubo(Q, num_reads = 10, label='Example - Simple Ocean Programs: QUBO')
+# print results
+print(sampleset)
