4da44fc1f84674e2a24640f1389a27c484f230fa
MichaelMeier2907 <100663243+MichaelMeier2907@users.noreply.github.com>
2022-03-14 09:36:44 +0100

    Update smallestsum.py
    gamma = 18

--- smallestsum.py
+++ smallestsum.py
@@ -2,16 +2,16 @@
 # objectiv = min(17 x0 + 19 X1 + 21 x2)
 # constraint (x0 + x1 + x2 - 2) = 0 or min((x0 + x1 + x2 - 2)2)
 # QUBO = min( (17 x0 + 19 x1 + 21 x2) + gamma (- 3 x0 - 3 x1 - 3 x2 + 2 x0 x1 + 2 x0 x2 + 2 x1 x2) )
-# gamma = 16
-# QUBO = min(17 x0 + 19 x1 + 21 x2 - 48 x0 - 48 x1 - 48 x2 + 34 x0 x1 + 34 x0 x2 + 34 x1 x2)
-# QUBO = min (-31 x0 -29 x1 -27 x2 + 32 x0 x1 + 32 x0 x2 + 32 x1 x2)
+# gamma = 18
+# QUBO = min(17 x0 + 19 x1 + 21 x2 - 54 x0 - 54 x1 - 54 x2 + 36 x0 x1 + 36 x0 x2 + 36 x1 x2)
+# QUBO = min (-37 x0 -35 x1 -33 x2 + 36 x0 x1 + 36 x0 x2 + 36 x1 x2)
 # runs on the exact solver
 # import
 import dimod
 # assign variable exactsolver
 exactsolver = dimod.ExactSolver()
-# QUBO a0 = -31, a1 = -29, a2 = -27, b01 = 32, b02 = 32 and b12 = 32
-Q = {(0,0):-31,(0,1):32,(0,2):32,(1,1):-29,(1,2):32,(2, 2):-27}
+# QUBO a0 = -37, a1 = -35, a2 = -33, b01 = 36, b02 = 36 and b12 = 36
+Q = {(0,0):-37,(0,1):36,(0,2):36,(1,1):-35,(1,2):36,(2, 2):-33}
 # assign results
 results = exactsolver.sample_qubo(Q)
 # print results
